913d0f4d535ac7dcb5cffa9ffaae0ebb46ee05e6
dnazaryan <dnazaryan@griddynamics.com>
2025-04-13 15:27:37 +0400

    app create

--- src/rest/rest.module.ts
+++ src/rest/rest.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './controller/auth/AuthController';
+import { CoreModule } from '../core/core.module';
 
-@Module({})
+@Module({
+  imports: [CoreModule],
+  controllers: [AuthController],
+})
 export class RestModule {}
